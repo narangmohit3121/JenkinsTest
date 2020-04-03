ef331eb32953cc55fc41e817e1c807a84a931779
“Mohit <Mohit.Narang@bts.com>
2020-04-03 22:31:27 +0530

    updated

--- JSfiles/app.js
+++ JSfiles/app.js
@@ -3,3 +3,4 @@ var x = "test:";
 for (var i = 1; i <= 10; i++) {
     console.log(x + i);
 }
+console.log('updated');
--- app.ts
+++ app.ts
@@ -3,3 +3,5 @@ let x:string = "test:";
 for(var i=1; i <= 10; i++){
     console.log(x + i );
 }
+
+console.log('updated');
